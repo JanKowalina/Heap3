f2013030edf21b42023a8fedae1f278242c1db29
Jan Kowalina <s184691@student.pg.edu.pl>
2023-07-08 23:06:15 +0200

    commented code

--- Heap3.cpp
+++ Heap3.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 void add(int heap[], int* leafCount, int value, int* oddEven) {
+    // Add a value to the heap
     int leafIndex = *leafCount;
     heap[leafIndex] = value;
 
+    // Adjust the heap to maintain the heap property
     while (leafIndex != 0) {
         if ((heap[(leafIndex - 1) / 2] < heap[leafIndex] && (*oddEven % 2) == 0) ||
             (heap[(leafIndex - 1) / 2] > heap[leafIndex] && (*oddEven % 2) == 1)) {
@@ -17,6 +19,7 @@ void add(int heap[], int* leafCount, int value, int* oddEven) {
 }
 
 void heapify(int heap[], int* leafCount, int index, int* oddEven) {
+    // Rearrange the heap to maintain the heap property
     int largest = index;
     int left = index * 2 + 1;
     int right = index * 2 + 2;
@@ -42,12 +45,14 @@ void heapify(int heap[], int* leafCount, int index, int* oddEven) {
 }
 
 void buildHeap(int heap[], int* leafCount, int* oddEven) {
+    // Build a heap from an array
     int firstLeaf = *leafCount / 2 - 1;
     for (int i = firstLeaf; i >= 0; i--)
         heapify(heap, leafCount, i, oddEven);
 }
 
 int remove(int heap[], int* leafCount, int* oddEven) {
+    // Remove and return the root element from the heap
     (*leafCount)--;
     int removed = heap[*leafCount];
     heap[*leafCount] = heap[0];
@@ -57,6 +62,7 @@ int remove(int heap[], int* leafCount, int* oddEven) {
 }
 
 void reverseHeap(int heap[], int* leafCount, int* oddEven) {
+    // Reverse the order of elements in the heap
     int firstLeaf = *leafCount / 2 - 1;
     for (int i = firstLeaf; i >= 0; i--)
         heapify(heap, leafCount, i, oddEven);
@@ -73,21 +79,25 @@ int main() {
         cin >> command;
         switch (command) {
             case '+':
+                // Add value to the heap
                 cin >> value;
                 add(heap, &leafCount, value, &oddEven);
                 leafCount++;
                 break;
             case '-':
+                // Remove and print the root element
                 if (leafCount != 0)
                     cout << remove(heap, &leafCount, &oddEven) << endl;
                 break;
             case 'p':
+                // Print the elements in the heap
                 for (int i = 0; i < leafCount; i++) {
                     cout << heap[i] << " ";
                 }
                 cout << endl;
                 break;
             case 'r':
+                // Read elements and build a heap
                 int length;
                 cin >> length;
                 if (length <= heapSize) {
@@ -99,10 +109,12 @@ int main() {
                 }
                 break;
             case 's':
+                // Reverse the heap order
                 oddEven++;
                 reverseHeap(heap, &leafCount, &oddEven);
                 break;
             case 'q':
+                // Free memory and exit the program
                 delete[] heap;
                 return 0;
                 break;
